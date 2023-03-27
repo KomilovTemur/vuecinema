343450862ea73075982828b4e9c318ad26cfd01f
Temur Komilov <83705446+KomilovTemur@users.noreply.github.com>
2023-03-27 07:57:26 +0000

    adding javascript to this project

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let nav_menu = document.querySelector(".nav__menu");
+let nav_list = document.querySelector(".nav__list");
+console.log(nav_menu);
+// nav_menu.addEventListener("click", () => {
+//   nav_list.classList.toggle("active");
+// })
